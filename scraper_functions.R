803c054193cf4d09a0cf87f731d29fc1bce8b157
Tim Cross <tim_cross@me.com>
2023-01-30 16:14:23 +0000

    get_stream_data migrated to upcoming stravR package

--- scraper_functions.R
+++ scraper_functions.R
@@ -62,66 +62,8 @@ ytd_stats <- tbl(con, "activities") %>%
 
 # Scraper functions -------------------------------------------------------
 
-get_stream_data <- function(activity_id, display_map = F) {
-  
-  str_glue("Starting activity {activity_id}.") %>% print()
-  
-  stream_types <- c("time", "latlng", "distance", "altitude", "velocity_smooth", "heartrate", "cadence", "watts", "temp", "moving", "grade_smooth") %>% 
-    str_flatten(collapse = ",")
-  
-  stream <- GET(url = str_glue("https://www.strava.com/api/v3/activities/{activity_id}/streams/{stream_types}"),
-                stoken)
-  
-  if(stream$status_code == 429) {
-    stop("Rate limit exceeded")
-  }
-  
-  stream_to_load <- stream$content %>%
-    rawToChar() %>%
-    jsonlite::fromJSON() %>% 
-    unnest(data) %>% 
-    group_by(type) %>% 
-    mutate(record_id = row_number()) %>% 
-    ungroup()
-  
-  if(any(stream_to_load$type == "latlng")) {
-    stream_to_load <- stream_to_load %>% 
-      mutate(value = data[,1])
-    
-    stream_to_load <- stream_to_load %>% 
-      select(record_id, type, value) %>% 
-      pivot_wider(names_from = "type") %>% 
-      rename(lat = latlng) %>% 
-      left_join(stream_to_load %>%
-                  filter(type == "latlng") %>% 
-                  transmute(record_id, lng = data[,2])) %>% 
-      select(-record_id) %>% 
-      mutate(id = activity_id)
-  } else {
-    stream_to_load <- stream_to_load %>% 
-      select(record_id, type, data) %>% 
-      pivot_wider(names_from = "type",
-                  values_from = "data") %>% 
-      select(-record_id) %>% 
-      mutate(id = activity_id)
-  }   
-  
-  if(display_map) {
-    
-    leaflet::leaflet() %>% 
-      leaflet::addTiles() %>% 
-      leaflet::addPolylines(stream_to_load$lng, stream_to_load$lat) %>% 
-      print()
-    
-  }
-  
-  dbWriteTable(con, "streams", stream_to_load, append = T)
-  
-  str_glue("Activity {activity_id} appended to database.") %>% print()
-  
-  Sys.sleep(8) # if loading multiple activities, prevents exceeding the 100 request in 15 minute rate limit
-  
-}
+# get_stream_data 
+source("~/Documents/Coding/R/Strava/stravR/get_stream_data.R")
 
 calculate_activity_peaks <- function(activity_id,
                                      peaks_for = c("cadence", "heartrate", "watts", "velocity_smooth"),

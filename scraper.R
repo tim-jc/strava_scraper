37a8b1b9acbb0799587150683393c12b91803445
tim-jc <tim_cross@me.com>
2026-06-08 08:41:43 +0100

    - logging added fro cron debugging - config.R replaced with .Renviron and deployed in main script

--- scraper.R
+++ scraper.R
@@ -2,27 +2,121 @@
 # retrieve new activities from Strava and 
 # add to MySQL database on RaspberryPi
 
-# libraries
+# clear memory ------------------------------------------------------------
+
+rm(list = ls(all = TRUE))
+
+# details for log file ----------------------------------------------------
+
+cat("\n=================================================\n")
+cat("SCRAPER START:", as.character(Sys.time()), "\n")
+cat("R VERSION:", R.version.string, "\n")
+cat("WORKING DIR:", getwd(), "\n")
+cat("HOME:", Sys.getenv("HOME"), "\n")
+cat("USER:", Sys.getenv("USER"), "\n")
+cat("RSTUDIO:", Sys.getenv("RSTUDIO"), "\n")
+cat("=================================================\n")
+flush.console()
+
+# runtime arguments -------------------------------------------------------
+
+args <- commandArgs(trailingOnly = TRUE)
+
+run_mode <- ifelse(
+  length(args) > 0,
+  args[1],
+  "manual"
+)
+
+cat(glue::glue(
+  "RUN MODE: {run_mode}\n"
+))
+
+flush.console()
+
+# libraries ---------------------------------------------------------------
+
 library(tidyverse)
 library(lubridate)
 library(DBI)
 library(slider)
+library(glue)
+library(here)
 library(stravR)
 
-# clear the memory
-rm(list=ls(all = TRUE))
+# project setup -----------------------------------------------------------
 
-# wd
-setwd("~/Documents/Coding/R/Strava/strava_scraper/")
+setwd(normalizePath("~/Documents/Coding/R/Strava/strava_scraper"))
 
-# establish project root
 here::i_am("scraper.R")
 
-# Functions
-here::here("config.R") %>% source()
+# environment -------------------------------------------------------------
+
+# load environment variables
+readRenviron(here::here(".Renviron"))
+
+# validate required environment variables
+assert_env_var <- function(x) {
+  val <- Sys.getenv(x)
+  if(val == "") {stop(glue::glue("Environment variable '{x}' not set."))}
+}
+
+required_env_vars <- c(
+  "STRAVA_APP_NAME",
+  "STRAVA_CLIENT_ID",
+  "STRAVA_APP_SECRET",
+  "DB_NAME",
+  "DB_HOST",
+  "DB_PORT",
+  "DB_USER",
+  "DB_PASSWORD",
+  "RSTUDIO_PANDOC"
+)
+
+purrr::walk(
+  required_env_vars,
+  assert_env_var
+)
+
+# set pandoc environment
+Sys.setenv(
+  RSTUDIO_PANDOC = Sys.getenv("RSTUDIO_PANDOC")
+)
+
+# config ------------------------------------------------------------------
+
+config <- list(
+  
+  strava = list(
+    app_name = Sys.getenv("STRAVA_APP_NAME"),
+    client_id = Sys.getenv("STRAVA_CLIENT_ID"),
+    app_secret = Sys.getenv("STRAVA_APP_SECRET")
+  ),
+  
+  db = list(
+    name = Sys.getenv("DB_NAME"),
+    host = Sys.getenv("DB_HOST"),
+    port = as.integer(Sys.getenv("DB_PORT")),
+    user = Sys.getenv("DB_USER"),
+    password = Sys.getenv("DB_PASSWORD")
+  )
+  
+)
+
+
+# source db functions -----------------------------------------------------
+
+here::here("db_helpers.R") %>% source()
+
+# database connection -----------------------------------------------------
+
+cat("Attempting DB connection...\n")
+
+con <- connect_to_db()
+
+# source main functions ---------------------------------------------------
+
 here::here("scraper_functions.R") %>% source()
-
-
 
 # Define values -----------------------------------------------------------
 
@@ -47,7 +141,10 @@ new_activities_to_load <- get_activity_data(strava_token = stoken,
 # Write new activities to DB
 if(nrow(new_activities_to_load) > 0) {
   dbWriteTable(con, "activities", new_activities_to_load, append = T)
-  ntfy_msg <- str_glue("{nrow(new_activities_to_load)} activit{if_else(nrow(new_activities_to_load) == 1,'y','ies')} loaded\n")
+  ntfy_msg <- str_glue(
+    "{nrow(new_activities_to_load)} activit{if_else(nrow(new_activities_to_load) == 1,'y','ies')} loaded\n",
+    "Run mode: {run_mode}\n",
+    "Completed: {Sys.time()}")
   }
 
 
@@ -84,3 +181,13 @@ publish_to_git()
 
 # Send notification
 send_ntfy_message(ntfy_msg)
+
+# Final messages for log
+
+cat("Disconnecting database connection...\n")
+flush.console()
+
+DBI::dbDisconnect(con)
+
+cat("Scraper complete.\n")
+flush.console()

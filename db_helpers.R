c6261820a2f803e7f6a44b7b30404362ed84cc36
tim-jc <tim_cross@me.com>
2026-06-08 08:40:49 +0100

    script created to separate out from main functions

---
+++ db_helpers.R
@@ -0,0 +1,46 @@
+connect_to_db <- function(max_attempts = 5,
+                          wait_seconds = 30) {
+  
+  for(i in seq_len(max_attempts)) {
+    
+    cat(glue::glue(
+      "DB connection attempt {i}/{max_attempts}\n"
+    ))
+    
+    con <- tryCatch(
+      
+      DBI::dbConnect(
+        RMariaDB::MariaDB(),
+        dbname = config$db$name,
+        host = config$db$host,
+        port = config$db$port,
+        user = config$db$user,
+        password = config$db$password
+      ),
+      
+      error = function(e) {
+        
+        cat("Connection failed:\n")
+        cat(conditionMessage(e), "\n")
+        
+        NULL
+        
+      }
+      
+    )
+    
+    if(!is.null(con)) {
+      
+      cat("DB connection successful.\n")
+      
+      return(con)
+      
+    }
+    
+    Sys.sleep(wait_seconds)
+    
+  }
+  
+  stop("Unable to connect to database.")
+  
+}

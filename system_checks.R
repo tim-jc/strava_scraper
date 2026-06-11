f8c242c61047172e3720041b3423603093b4c8bc
tim-jc <65184272+tim-jc@users.noreply.github.com>
2026-06-11 17:21:53 +0100

    Update system_checks.R

--- system_checks.R
+++ system_checks.R
@@ -1,15 +1,32 @@
 check_cron_schedule <- function() {
 
-environ_schedule <- Sys.getenv("SCRAPER_SCHEDULE")
+  environ_schedule <- Sys.getenv("SCRAPER_SCHEDULE")
 
-# read schedule from cron
-cron_schedule <- tryCatch(
+  cron <- tryCatch(
     system("crontab -l", intern = TRUE),
     error = function(e) character(0)
   )
 
-# extract scraper schedule from cron
+  start_index <- match(
+    "# >>> STRAVA_SCRAPER_START >>>",
+    cron
+  )
 
-# compare, warn if out of sync
+  if(is.na(start_index)) {
+    return(FALSE)
+  }
+
+  cron_line <- cron[start_index + 2]
+
+  cron_schedule <- str_split_1(
+    cron_line,
+    "\\s+"
+  ) |>
+    (\(x) paste(x[1:5], collapse = " "))()
+
+  identical(
+    environ_schedule,
+    cron_schedule
+  )
 
 }

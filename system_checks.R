d93d5c5dfcf29e5f331265bf8832a77cc3499e4d
tim-jc <65184272+tim-jc@users.noreply.github.com>
2026-06-11 08:33:13 +0100

    Update system_checks.R

--- system_checks.R
+++ system_checks.R
@@ -3,7 +3,12 @@ check_cron_schedule <- function() {
 environ_schedule <- Sys.getenv("SCRAPER_SCHEDULE")
 
 # read schedule from cron
-cron_schedule 
+cron_schedule <- tryCatch(
+    system("crontab -l", intern = TRUE),
+    error = function(e) character(0)
+  )
+
+# extract scraper schedule from cron
 
 # compare, warn if out of sync
 
